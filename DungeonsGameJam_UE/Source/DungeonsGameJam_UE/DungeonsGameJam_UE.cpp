d0b7e92afb37d45ba24c0953ae499592dc654ba7
Lukasz Dziedziczak <wukash@gmail.com>
2025-03-16 18:57:11 +1030

    Removed Unreal

--- DungeonsGameJam_UE/Source/DungeonsGameJam_UE/DungeonsGameJam_UE.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DungeonsGameJam_UE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DungeonsGameJam_UE, "DungeonsGameJam_UE" );
