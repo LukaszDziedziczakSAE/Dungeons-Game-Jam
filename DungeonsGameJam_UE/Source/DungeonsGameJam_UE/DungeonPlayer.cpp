e445ef412c59be91fb5a2539566187d55bd100e8
Lukasz Dziedziczak <1031324@student.sae.edu.au>
2025-03-17 10:48:23 +1030

    Added Characters

---
+++ DungeonsGameJam_UE/Source/DungeonsGameJam_UE/DungeonPlayer.cpp
@@ -0,0 +1,5 @@
+// Created by Lukasz Dziedziczak. Copyright 2025
+
+
+#include "DungeonPlayer.h"
+
---
+++ DungeonsGameJam_UE/Source/DungeonsGameJam_UE/DungeonPlayer.h
@@ -0,0 +1,17 @@
+// Created by Lukasz Dziedziczak. Copyright 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DungeonCharacter.h"
+#include "DungeonPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONSGAMEJAM_UE_API ADungeonPlayer : public ADungeonCharacter
+{
+	GENERATED_BODY()
+	
+};
